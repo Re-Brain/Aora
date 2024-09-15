1f89ce2762dc721b3876fafecd47d48ddefabc0c
Orrelav <82299511+Re-Brain@users.noreply.github.com>
2024-09-15 16:53:18 +0800

    Finished sign-in & sign-up

---
+++ application/lib/appwrite.js
@@ -0,0 +1,8 @@
+export const appwriteConfig = {
+    endpoint : 'https://clould.appwrite.io.v1',
+    platform : 'com.jsm.aora',
+    projectId : '66e68f5200103cc7a5ec',
+    databaseId : '66e690e90017f746277f',
+    userCollectionId : '66e69128002b91cdc04c',
+    storageId: '66e69e5a000661f5dd03'
+}
